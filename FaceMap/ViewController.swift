9bd8fdeb28d41990c91b8444f8ca15c5e3598752
Vu Truong <chubBiTi@vus-mbp-2.dynamic.ucsd.edu>
2016-04-15 18:53:57 -0700

    Commit to branch VT

--- FaceMap/ViewController.swift
+++ FaceMap/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         print("Hello");
+        print("Commiting to Vu");
     }
 
     override func didReceiveMemoryWarning() {
