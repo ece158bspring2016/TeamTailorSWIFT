0c5f6045d00727b235d82695f72cd5b051b0929f
DanHy Do <danhydo@me.com>
2016-04-19 15:05:37 -0700

    testing

--- FaceMap/ViewController.swift
+++ FaceMap/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         print("Hello");
+    
+        
     }
 
     override func didReceiveMemoryWarning() {
