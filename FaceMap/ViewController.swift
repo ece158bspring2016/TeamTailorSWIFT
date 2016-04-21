c87e1e4ffed92f31608882c92fba3f34430952c0
Vu Truong <chubBiTi@vus-mbp-2.attlocal.net>
2016-04-20 17:59:23 -0700

    First commit from Vu Truong

--- FaceMap/ViewController.swift
+++ FaceMap/ViewController.swift
@@ -8,20 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIScrollViewDelegate {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        print("Hello");
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
 
