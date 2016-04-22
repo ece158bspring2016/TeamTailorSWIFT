03506b7a531e6e7d1d3de7a8f2faf80ab1220b53
Vu Truong <chubBiTi@vus-macbook-pro-2.local>
2016-04-22 15:48:13 -0700

    Update story board and write some code in the UIswift

--- FaceMap/ViewController.swift
+++ FaceMap/ViewController.swift
@@ -13,9 +13,37 @@ class ViewController: UIViewController, UIScrollViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        print("Hello");
-        print("Commiting to Vu");
+        enum Icon{
+            case happy
+            case veryhappy
+            case sad
+            case verysad
+            case verysad2
+            case scared
+            case frighten
+            case angry
+            case furious
+            case depressed
+            case excited
+            
+            var image: UIImage {
+                switch self {
+                case .happy: return UIImage (named: "happy.png")!
+                case .veryhappy: return UIImage (named: "veryhappy.png")!
+                case .sad: return UIImage (named: "sad.png")!
+                case .verysad: return UIImage (named: "verysad.png")!
+                case .verysad2: return UIImage (named: "verysad2.png")!
+                case .scared: return UIImage (named: "scared.png")!
+                case .frighten: return UIImage  (named: "frighten.png")!
+                case .angry: return UIImage (named: "angry.png")!
+                case .furious: return UIImage (named: "furiouspng")!
+                case .depressed: return UIImage (named: "depressed.png")!
+                case .excited: return UIImage (named: "excited.png")!
+                    
+                }
+            }
+        }
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,4 +52,3 @@ class ViewController: UIViewController, UIScrollViewDelegate {
     }
     
 }
-
