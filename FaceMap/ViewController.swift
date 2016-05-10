74ede1c7f91a4bf4e3ecdd555693376e2d3df835
Vu Truong <chubBiTi@vus-macbook-pro-2.local>
2016-05-10 14:20:39 -0700

    Login and register page are done. We should replace the code for saving into the phone's data base with the cloud database.

--- FaceMap/ViewController.swift
+++ FaceMap/ViewController.swift
@@ -10,41 +10,84 @@ import UIKit
 
 class ViewController: UIViewController, UIScrollViewDelegate {
     
+    // for transition between on/off grid button
+    @IBAction func Ongridbutton(sender: AnyObject) {
+        self.performSegueWithIdentifier("ongridtoemoji1", sender: sender)
+    }
+    @IBAction func offgridbutton(sender: AnyObject) {
+        self.performSegueWithIdentifier("offgridtoemoji2", sender: sender)
+    }
+    
+    // for transistion between mood and interest in on
+    @IBAction func happyimage(sender: UIButton) {
+        self.performSegueWithIdentifier("emojitointerest1", sender: sender)
+    }
+    @IBAction func veryhappyimage(sender: UIButton) {
+        self.performSegueWithIdentifier("emojitointerest1", sender: sender)
+    }
+    @IBAction func sadimage(sender: UIButton) {
+        self.performSegueWithIdentifier("emojitointerest1", sender: sender)
+    }
+    @IBAction func verysadimage(sender: UIButton) {
+        self.performSegueWithIdentifier("emojitointerest1", sender: sender)
+    }
+    @IBAction func scaredimage(sender: UIButton) {
+        self.performSegueWithIdentifier("emojitointerest1", sender: sender)
+    }
+    @IBAction func frightenimage(sender: AnyObject) {
+        self.performSegueWithIdentifier("emojitointerest1", sender: sender)
+    }
+    @IBAction func furious(sender: AnyObject) {
+        self.performSegueWithIdentifier("emojitointerest1", sender: sender)
+    }
+    @IBAction func angry(sender: AnyObject) {
+        self.performSegueWithIdentifier("emojitointerest1", sender: sender)
+    }
+    @IBAction func depressed(sender: AnyObject) {
+        self.performSegueWithIdentifier("emojitointerest1", sender: sender)
+    }
+    @IBAction func excited(sender: AnyObject) {
+        self.performSegueWithIdentifier("emojitointerest1", sender: sender)
+    }
+    
+    // transistion between mood and interest in off
+    @IBAction func happy1(sender: AnyObject) {
+        self.performSegueWithIdentifier("emoji2tointerest2", sender: sender)
+    }
+    @IBAction func veryhappy1(sender: AnyObject) {
+        self.performSegueWithIdentifier("emoji2tointerest2", sender: sender)
+    }
+    @IBAction func sad1(sender: AnyObject) {
+        self.performSegueWithIdentifier("emoji2tointerest2", sender: sender)
+    }
+    @IBAction func verysad1(sender: AnyObject) {
+        self.performSegueWithIdentifier("emoji2tointerest2", sender: sender)
+    }
+    @IBAction func scared(sender: AnyObject) {
+        self.performSegueWithIdentifier("emoji2tointerest2", sender: sender)
+    }
+    @IBAction func frighten1(sender: AnyObject) {
+        self.performSegueWithIdentifier("emoji2tointerest2", sender: sender)
+    }
+    @IBAction func furious1(sender: AnyObject) {
+        self.performSegueWithIdentifier("emoji2tointerest2", sender: sender)
+    }
+    @IBAction func angry1(sender: AnyObject) {
+        self.performSegueWithIdentifier("emoji2tointerest2", sender: sender)
+    }
+    @IBAction func depressed1(sender: AnyObject) {
+        self.performSegueWithIdentifier("emoji2tointerest2", sender: sender)
+    }
+    @IBAction func excited1(sender: AnyObject) {
+        self.performSegueWithIdentifier("emoji2tointerest2", sender: sender)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        enum Icon{
-            case happy
-            case veryhappy
-            case sad
-            case verysad
-            case verysad2
-            case scared
-            case frighten
-            case angry
-            case furious
-            case depressed
-            case excited
-            
-            var image: UIImage {
-                switch self {
-                case .happy: return UIImage (named: "happy.png")!
-                case .veryhappy: return UIImage (named: "veryhappy.png")!
-                case .sad: return UIImage (named: "sad.png")!
-                case .verysad: return UIImage (named: "verysad.png")!
-                case .verysad2: return UIImage (named: "verysad2.png")!
-                case .scared: return UIImage (named: "scared.png")!
-                case .frighten: return UIImage  (named: "frighten.png")!
-                case .angry: return UIImage (named: "angry.png")!
-                case .furious: return UIImage (named: "furiouspng")!
-                case .depressed: return UIImage (named: "depressed.png")!
-                case .excited: return UIImage (named: "excited.png")!
-                    
-                }
-            }
-        }
-
-    }
+        
+           }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
